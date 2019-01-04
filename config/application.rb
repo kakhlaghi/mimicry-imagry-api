7c20fac274ced1b800df729bc1cd01104493780f
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-01-04 13:10:20 -0800

    adds middleware for cors

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,10 @@ module MimicryImagryApi
     config.api_only = true
   end
 end
+
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3000'
+    resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+  end
+end
