0c792e0046b76ce0a188eff35aa0beefb6eeece8
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-01-08 07:25:35 -0800

    works on logging in

--- app/controllers/api/user_token_controller.rb
+++ app/controllers/api/user_token_controller.rb
@@ -1,5 +1,27 @@
 class Api::UserTokenController < Knock::AuthTokenController
 
+    def create
+        user = User.find_by(user: params[:user])
+        if user & user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            render json: {
+                jwt: encode_token({id: user.id, name: user.name})
+            }
+        end
+    end
     
+    private
+
+    def encode_token(payload = {})
+      exp= 24.hours.from_now
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+ 
+    end
+ 
+    def decode_token(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base)
+   end
+
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::API
     include Knock::Authenticable
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
+
   private
   # Define unauthorized access json response
       def unauthorized_entity(entity_name)
           render json: { error: "Unauthorized request" }, status:     
           :unauthorized
       end
+
+
 end
